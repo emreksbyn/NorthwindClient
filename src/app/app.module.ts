9aaed6c13b2199311aa78504c6be230a8f3b7ff2
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-08 19:13:38 +0300

    login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ProductAddComponent } from './components/product-add/product-add.compon
     FilterPipePipe,
     CartSummaryComponent,
     ProductAddComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
